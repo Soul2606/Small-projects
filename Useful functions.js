fcca0249499f0340c11bc61551d6a334807800fb
Sondre Ulvik <sondulv@gmail.com>
2024-06-29 19:19:41 +0200

    distribute_evenly_within_range better implementation

--- Useful functions.js
+++ Useful functions.js
@@ -70,112 +70,54 @@ function filter(input, filter_function){
 
 function distribute_evenly_within_range(value, ranges){
     let results = []
-    let ranges_index_value_pair = []
-    for (let i = 0; i < ranges.length; i++) {
-        const element = ranges[i];
-        ranges_index_value_pair.push([i, element])
-        results.push(0)
-    }
-    const loop_length = ranges_index_value_pair.length
-    for (let i = 0; i < loop_length; i++) {
-        console.group(i)
-
-        if (ranges_index_value_pair.length === 0) {
-            console.groupEnd()
-            return results
-        }
-
-        let lowest_value = {value:Infinity, ranges_index_value_pair_indexes:[]}
-        for (let j = 0; j < ranges_index_value_pair.length; j++) {
-            const index = ranges_index_value_pair[j][0]
-            const value = ranges_index_value_pair[j][1]
-            if(lowest_value.value > value - results[index]){
-                lowest_value.value = value - results[index]
-                lowest_value.ranges_index_value_pair_indexes.push(j)
-            }else if(lowest_value.value === value - results[index]){
-                lowest_value.ranges_index_value_pair_indexes.push(j)
-            }
-        }
-
-        
-        console.log("ranges_index_value_pair", Array.from(ranges_index_value_pair))
-        console.log("lowest_value", lowest_value.value, "indexes", lowest_value.ranges_index_value_pair_indexes)
-        console.log("value", value)
-
-
-        const value_per_item = value / ranges_index_value_pair.length
-        console.log("value_per_item", value_per_item)
-        if (value_per_item <= lowest_value.value) {
-            console.log("amount added to each", value_per_item)
-            for (let i = 0; i < ranges_index_value_pair.length; i++) {
-                const index = ranges_index_value_pair[i][0]
-                results[index] += value_per_item
-            }
-            console.groupEnd()
-            return results
-        }
-
-        console.log("amount added to each", lowest_value.value)
-
-        for (let i = 0; i < ranges_index_value_pair.length; i++) {
-            const index = ranges_index_value_pair[i][0]
-            results[index] += lowest_value.value
-            value -= lowest_value.value
-        }
-
-
-        for (let i = 0; i < lowest_value.ranges_index_value_pair_indexes.length; i++) {
-            const index = lowest_value.ranges_index_value_pair_indexes[i];
-            ranges_index_value_pair[index] = "pending_deletion"
-        }
-
-        for (let i = 0; i < ranges_index_value_pair.length; i++) {
-            const element = ranges_index_value_pair[i];
-            if (element === "pending_deletion") {
-                ranges_index_value_pair.splice(i, 1)
-                i--
-            }
-        }
-
-        console.groupEnd()
-    }   
-    return results
-}
-
-
-
-//Attempt 2
-function distribute_evenly_within_range(value, ranges){
-    let results = []
     let indexes_to_exclude = []
+    let highest_number_added_to_results = 0 
+    const ranges_unique_sorted = [...new Set(ranges)].sort((a, b) => a - b)
 
     for (let i = 0; i < ranges.length; i++) {
         results.push(0)
     }
 
-    for (let i = 0; i < ranges.length; i++) {
+    for (let i = 0; i < ranges_unique_sorted.length; i++) {
+        const lowest_value = ranges_unique_sorted[i]
+
+        const lowest_value_difference_to_highest_value_added = (lowest_value - highest_number_added_to_results)
         
-        let lowest_value = Infinity
-        for (let i = 0; i < ranges.length; i++) {
-            const element = ranges[i];
-            if (element < lowest_value) {
-                lowest_value = element
-            }
-        }
+        const value_per_item = value / (ranges.length - indexes_to_exclude.length)
+        
+        console.log("lowest_value_difference_to_highest_value_added", lowest_value_difference_to_highest_value_added, "lowest_value", lowest_value, "highest_number_added_to_results", highest_number_added_to_results, "\nindexes_to_exclude", indexes_to_exclude, "value_per_item", value_per_item)
 
-        const value_per_item = value / ranges.length
+        if (value_per_item <= lowest_value_difference_to_highest_value_added){
 
-        if (value_per_item <= lowest_value){
-
-            for (let i = 0; i < ranges.length; i++) {
+            for (let i = 0; i < results.length; i++) {
+                if(indexes_to_exclude.includes(i)){
+                    continue
+                }
                 results[i] += value_per_item
             }
 
             return results
         }
 
+        
+        for (let i = 0; i < results.length; i++) {
+            if(indexes_to_exclude.includes(i)){
+                continue
+            }
+            results[i] += lowest_value_difference_to_highest_value_added
+            value -= lowest_value_difference_to_highest_value_added
+            if (ranges[i] === lowest_value) {
+                indexes_to_exclude.push(i)
+            }
+        }
+
+        highest_number_added_to_results += lowest_value_difference_to_highest_value_added
     }
+
+    return results
 }
+
+console.log(distribute_evenly_within_range(23,[1,4,8,12]))
 
 
 
