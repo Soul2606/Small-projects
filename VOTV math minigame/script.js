2abc6eddb6965461adbd76864e72c03d4e313f52
Sondre Ulvik <sondulv@gmail.com>
2024-10-01 12:29:50 +0200

    finished VOTV mini-game

--- VOTV math minigame/script.js
+++ VOTV math minigame/script.js
@@ -10,40 +10,42 @@ const answer_button_8 = document.getElementById('answer-button-8')
 const answer_button_9 = document.getElementById('answer-button-9')
 const problem_paragraph = document.getElementById('problem')
 
+const all_answer_buttons = [answer_button_0,answer_button_1,answer_button_2,answer_button_3,answer_button_4,answer_button_5,answer_button_6,answer_button_7,answer_button_8,answer_button_9]
+
 let left
 let right
 let operator
-const stop_flash = {stop:true}
+let flash_kill_switch = {kill:undefined}
 
 answer_button_0.addEventListener('click', ()=>{
-    submit_answer(0, answer_button_0.firstElementChild)
+    submit_answer(0)
 })
 answer_button_1.addEventListener('click', ()=>{
-    submit_answer(1, answer_button_1.firstElementChild)
+    submit_answer(1)
 })
 answer_button_2.addEventListener('click', ()=>{
-    submit_answer(2, answer_button_2.firstElementChild)
+    submit_answer(2)
 })
 answer_button_3.addEventListener('click', ()=>{
-    submit_answer(3, answer_button_3.firstElementChild)
+    submit_answer(3)
 })
 answer_button_4.addEventListener('click', ()=>{
-    submit_answer(4, answer_button_4.firstElementChild)
+    submit_answer(4)
 })
 answer_button_5.addEventListener('click', ()=>{
-    submit_answer(5, answer_button_5.firstElementChild)
+    submit_answer(5)
 })
 answer_button_6.addEventListener('click', ()=>{
-    submit_answer(6, answer_button_6.firstElementChild)
+    submit_answer(6)
 })
 answer_button_7.addEventListener('click', ()=>{
-    submit_answer(7, answer_button_7.firstElementChild)
+    submit_answer(7)
 })
 answer_button_8.addEventListener('click', ()=>{
-    submit_answer(8, answer_button_8.firstElementChild)
+    submit_answer(8)
 })
 answer_button_9.addEventListener('click', ()=>{
-    submit_answer(9, answer_button_9.firstElementChild)
+    submit_answer(9)
 })
 
 
@@ -53,40 +55,63 @@ async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
 }
 
+
+
+
 function generate_problem(){
     left = Number.parseInt(Math.random() * 8) + 1
     right = Number.parseInt(Math.random() * 8) + 1 
     operator = Math.random() < 0.5? '+': '-'
-    problem_paragraph.textContent = 'mod(|'+left+' '+operator+' '+right+'|)'
+    problem_paragraph.textContent = '|'+left+' '+operator+' '+right+'| mod 10'
 }
 
-function submit_answer(answer, element){
+
+
+
+function submit_answer(answer){
+    flash_kill_switch.kill = true
     if(operator === '+'){
-        console.log(answer === Math.abs(left + right) % 10)
-        flash(element, true)
+        const solution = Math.abs(left + right) % 10
+        flash_kill_switch = flash(all_answer_buttons[solution].firstElementChild, answer === solution)
     }else{
-        console.log(answer === Math.abs(left - right) % 10)
-        flash(element, false)
+        const solution = Math.abs(left - right) % 10
+        flash_kill_switch = flash(all_answer_buttons[solution].firstElementChild, answer === solution)
     }
     generate_problem()
 }
 
-async function flash(element, green) {
 
-    if(green){
-        element.style.color = 'rgb(0,255,0)'
-    }else{
-        element.style.color = 'rgb(255,0,0)'
-    }
-    for (let i = 0; i < 255; i++) {
-        await sleep(5)
-        
-        if (green) {
-            element.style.color = 'rgb('+i+',255,'+i+')'
+
+
+function flash(element, green) {
+
+    const kill_this_flash = {kill:false}
+
+    const actual_flash = async (element, green)=>{
+        if(green){
+            element.style.color = 'rgb(0,255,0)'
         }else{
-            element.style.color = 'rgb(255,'+i+','+i+')'
+            element.style.color = 'rgb(255,0,0)'
+        }
+        for (let i = 0; i < 255; i++) {
+            await sleep(5)
+            
+            if(kill_this_flash.kill){
+                element.style.color = ''
+                break
+            }
+
+            if (green) {
+                element.style.color = 'rgb('+i+',255,'+i+')'
+            }else{
+                element.style.color = 'rgb(255,'+i+','+i+')'
+            }
         }
     }
+
+    actual_flash(element, green)
+
+    return kill_this_flash
 }
 
 generate_problem()
